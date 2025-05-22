4ee8ab3d4d5a78ef3a1d208b2c5d20ec64851639
ne1ntrovert <shironovilya777@gmail.com>
2025-05-22 17:09:46 +0300

    cursovaia2.1

--- tests/test_hh_api.py
+++ tests/test_hh_api.py
@@ -39,37 +39,3 @@ class TestHHGateway(unittest.TestCase):
         jobs = gateway.fetch("Python", 1)
         self.assertEqual(len(jobs), 1)
         self.assertIsInstance(jobs[0], JobInfo)
- 29 changes: 29 additions & 0 deletions29
-tests/test_savers.py
-Viewed
-Original file line number	Diff line number	Diff line change
-@@ -0,0 +1,29 @@
-import os
-import unittest
-
-from src.savers import JSONHandler
-from src.vacancy import JobInfo
-
-
-class TestJSONHandler(unittest.TestCase):
-    def setUp(self):
-        self.path = "test_data.json"
-        self.handler = JSONHandler(path=self.path)
-        self.handler.clear()
-
-    def tearDown(self):
-        if os.path.exists(self.path):
-            os.remove(self.path)
-
-    def test_write_and_read(self):
-        job = JobInfo("Tester", "QA Ltd", 60000, 80000, "RUB", "test_link")
-        self.handler.write([job])
-        data = self.handler.read()
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[0].name, "Tester")
-
-    def test_clear(self):
-        self.handler.write([JobInfo("Test", "Emp", 1, 2, "RUB", "link")])
-        self.handler.clear()
-        data = self.handler.read()
-        self.assertEqual(data, [])
